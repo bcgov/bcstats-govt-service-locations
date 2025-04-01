e8264cc3fb620f30c67ed4e294ad4526413c093a
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2025-03-31 21:12:46 -0700

    add boilerplate

--- Python/00-make_data.py
+++ Python/00-make_data.py
@@ -1,3 +1,23 @@
+# Copyright 2025 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script loads raw csv data files containing spatial data for addresses in 
+# a defined municipality in BC (loc).  The datasets are processed to be ready 
+# for use for data analytics. 
+
+
+
 import os
 import glob
 import pandas as pd
