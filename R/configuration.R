661550ef3eb7999a13f40e265c5bebb027c3c8b0
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2025-04-04 11:45:02 -0700

    minor changes

--- R/configuration.R
+++ R/configuration.R
@@ -15,12 +15,15 @@
 # This script is used for defining variables global to the project.
 # required libraries are defined here as well.
 
+#------------------------------------------------------------------------------
+# Load Reqd Libraries
+#------------------------------------------------------------------------------
+
 library(tidyverse)
 library(safepaths)
 library(glue)
 library(janitor)
 library(e1071)
-
 library(sf)
 
 # library(cancensus) # nolint
@@ -36,16 +39,26 @@ library(sf)
 # Load the rlang package for the bang-bang operator - imported by cowplot
 # library(rlang) # nolint
 
+#------------------------------------------------------------------------------
+# Set Options
+#------------------------------------------------------------------------------
 
 # set timeout on file load process
 getOption("timeout")
 options(timeout = 600)
 
-loc_list <- c("909", "227", "213", "420")
+#------------------------------------------------------------------------------
+# Declare Global Constants
+#------------------------------------------------------------------------------
+EXPECTED_LOCALITIES <- c("909", "227", "213", "420")
 
-lan_folder <- safepaths::use_network_path()
-src_data_folder <- glue::glue("{lan_folder}/data/source/")
-raw_data_folder <- glue::glue("{lan_folder}/data/raw/")
+LAN_FOLDER <- use_network_path()
+SRC_DATA_FOLDER <- glue("{lan_folder}/data/source/")
+RAW_DATA_FOLDER <- glue("{lan_folder}/data/raw/")
 
+NO_ERRS_FILE_PATTERN <- "no_errors.csv"
+LOCALITY_REGEX_PATTERN <- "[0-9][0-9][0-9]"
 
+REQUIRED_COLS <- c("site_albers_x", "site_albers_y", "dissemination_block_id", "drv_time_sec", "drv_dist", "tag")
+FACILITY_TAG <- "servicebc"
 
