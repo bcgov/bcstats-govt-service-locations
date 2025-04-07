4427ea3e7c51f1e3b7fc3a7285f22760dd5671c7
Bonnie Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2025-04-07 11:16:22 -0700

    reformat to make function list more clear

--- R/fxns/calculate-stats.r
+++ R/fxns/calculate-stats.r
@@ -12,36 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# ------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------
+
 
 # Helper function to calculate summary statistics
 calculate_drivetime_stats <- function(df, group_cols) {
 
+  numeric_stats <- list(
+    mean    = ~ mean(.x, na.rm = TRUE),
+    var     = ~ var(.x, na.rm = TRUE),
+    kurt    = ~ e1071::kurtosis(.x, na.rm = TRUE, type = 1),
+    skew    = ~ e1071::skewness(.x, na.rm = TRUE, type = 1),
+    qnt0    = ~ quantile(.x, probs = 0.00, na.rm = TRUE), # or min(.x, na.rm = TRUE)
+    qnt25   = ~ quantile(.x, probs = 0.25, na.rm = TRUE),
+    qnt50   = ~ quantile(.x, probs = 0.50, na.rm = TRUE), # or median(.x, na.rm = TRUE)
+    qnt75   = ~ quantile(.x, probs = 0.75, na.rm = TRUE),
+    qnt100  = ~ quantile(.x, probs = 1.00, na.rm = TRUE) # or max(.x, na.rm = TRUE)
+  )
+
   df %>%
     group_by(across(all_of(group_cols))) %>%
+    # Apply stats to specified numeric variables
     summarise(
-      mn_drv_time_sec = mean(drv_time_sec, na.rm = TRUE),
-      mn_drv_dist = mean(drv_dist, na.rm = TRUE),
-      qnt0_drv_time_sec = quantile(drv_time_sec, probs = 0, na.rm = TRUE),
-      qnt1_drv_time_sec = quantile(drv_time_sec, probs = 0.25, na.rm = TRUE),
-      qnt2_drv_time_sec = quantile(drv_time_sec, probs = 0.5, na.rm = TRUE),
-      qnt3_drv_time_sec = quantile(drv_time_sec, probs = 0.75, na.rm = TRUE),
-      qnt4_drv_time_sec = quantile(drv_time_sec, probs = 1, na.rm = TRUE),
-      qnt0_drv_dist = quantile(drv_dist, probs = 0, na.rm = TRUE),
-      qnt1_drv_dist = quantile(drv_dist, probs = 0.25, na.rm = TRUE),
-      qnt2_drv_dist = quantile(drv_dist, probs = 0.5, na.rm = TRUE),
-      qnt3_drv_dist = quantile(drv_dist, probs = 0.75, na.rm = TRUE),
-      qnt4_drv_dist = quantile(drv_dist, probs = 1, na.rm = TRUE),
-      var_drv_time_sec = var(drv_time_sec, na.rm = TRUE),
-      var_drv_dist = var(drv_dist, na.rm = TRUE),
-      # Ensure moments package is loaded or use moments::skewness etc.
-      skw_drv_time_sec = skewness(drv_time_sec, na.rm = TRUE, type = 1), 
-      skw_drv_dist = skewness(drv_dist, na.rm = TRUE, type = 1),
-      kurt_drv_time_sec = kurtosis(drv_time_sec, na.rm = TRUE, type = 1),
-      kurt_drv_dist = kurtosis(drv_dist, na.rm = TRUE, type = 1),
-      n_address = n_distinct(fid), # Make sure 'fid' exists in 'data'
-      .groups = "drop" # Automatically ungroups after summarise
-    )
+      across(c(drv_dist, drv_time_sec), numeric_stats, .names = "{col}_{fn}"),
+      # add calculations that apply to the whole group
+      n_address = n_distinct(fid),
+      .groups = "drop")
 }
