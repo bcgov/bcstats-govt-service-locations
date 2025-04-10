0d7baf10c6b3b68d86f8eca9f2049dbf0ae1f489
Bonnie Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2025-04-10 09:35:19 -0700

    new script to pre-process shapefiles

--- R/03-make-map-data.R
+++ R/03-make-map-data.R
@@ -1,47 +1,61 @@
 library(tidyverse)
 library(glue)
 library(janitor)
-library(e1071)
 library(sf)
 
 source("R/settings.R")
-source("R/fxns/pre-processing.R")
-source("R/fxns/calculations.R")
 
-da_file_path <-  glue("{RAW_DATA_FOLDER}/statscan/lda_000b21a_e/lda_000b21a_e.shp")
-db_file_path <-  glue("{RAW_DATA_FOLDER}/statscan/ldb_000b21a_e/ldb_000b21a_e.shp")
-
-da_shapefile <- st_read(da_file_path)
-da_shapefile <- da_shapefile %>%
-  filter(PRUID == '59') %>%
-  st_transform(crs = 3005)
-
-#  %>%
-#  clean_names() %>%
-# select(daid = dauid, landarea, geometry) # Select relevant columns
-
-data <- read_csv(glue("{SRC_DATA_FOLDER}/da_average_times_dist_all_locs.csv")) %>%
-  clean_names() %>%
-  mutate(across(c(daid, loc), as.character)) %>%
-  filter(loc == "213") %>% # Filter for locality 213
-  select(daid, loc, drv_dist_mean)
-
-
-# read in shape files (db and da)
-
-# filter on db's in our localities (need a mapping table)
-
-db_shapefile <- st_read(db_file_path)
-db_shapefile <- db_shapefile %>%
-  filter(PRUID == '59') %>%
-  st_transform(crs = 3005)
-
-# write to source folder
+crosswalk_file_path <- glue("{SRC_DATA_FOLDER}/da-db-loc-crosswalk.csv")
+da_shapefile_path <-  glue("{RAW_DATA_FOLDER}/statscan/lda_000b21a_e/lda_000b21a_e.shp")
+da_shapefile_out <- glue("{SRC_DATA_FOLDER}/processed_da_shapefiles.shp")
+# db_file_path <-  glue("{RAW_DATA_FOLDER}/statscan/ldb_000b21a_e/ldb_000b21a_e.shp") lets do da only for now
 
 
 
-# filter on da's in our localities (need a mapping table)
+# -----------------------------------------------------------------------------------------
+# Load and prepare da-db-location crosswalk file
+# -----------------------------------------------------------------------------------------
 
+crosswalk <- tryCatch({
+  read_csv(crosswalk_file_path, col_types = cols(.default = "c")) %>%
+    distinct(daid, location_id)
+}, error = function(e) {
+  stop(glue("Failed to read or process crosswalk file '{crosswalk_file_path}': {e$message}"))
+})
 
+if (nrow(crosswalk) == 0) {
+  stop("Crosswalk data is empty after processing.")
+}
 
-# 
+# -----------------------------------------------------------------------------------------
+# Load and prepare da shapefile - check for invalid data
+# -----------------------------------------------------------------------------------------
+
+da_shapefiles_processed <- tryCatch({
+    st_read(da_shapefile_path) %>%
+    filter(PRUID == "59") %>%
+    st_transform(crs = 3005) %>%
+    clean_names() %>%
+    select(daid = dauid, landarea, geometry)
+}, error = function(e) {
+  stop(glue("Failed to read or process da shapefile '{da_shapefile_path}': {e$message}"))
+})
+
+if (nrow(da_shapefiles_processed) == 0) {
+  stop("DA shapefile data is empty after filtering for BC.")
+}
+
+# -----------------------------------------------------------------------------------------
+# Filter da shapefile to include only those da's in our localities of interest
+# could do some light error checking here, but we don't have a true mapping from
+# location to da (since locality is still undefinded and we don't have shapefiles for those)
+# -----------------------------------------------------------------------------------------
+
+test <- da_shapefiles_processed  %>%
+   inner_join(crosswalk, by = "daid")
+
+# -----------------------------------------------------------------------------------------
+# write processed da shapefiles to source folder
+# -----------------------------------------------------------------------------------------
+st_write(da_with_location, output_gpkg_path, delete_layer = TRUE)
+
