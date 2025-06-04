04ecb8e29089b8eb423266f2b3421f78c733331f
Bonnie Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2025-06-04 12:49:26 -0700

    refactoring

--- Python/00-make_data.py
+++ Python/00-make_data.py
@@ -16,63 +16,71 @@
 # a defined municipality in BC (loc).  The datasets are processed to be ready 
 # for use for data analytics. 
 
+#------------------------------------------------------------------------------
+# Import req'd modules, libraries and source constants
+#------------------------------------------------------------------------------
+
 import os
 import glob
 import pandas as pd
 import re
+from Python.modules.safepaths import *
+
 
 #------------------------------------------------------------------------------
-# geodata team creates drive times files for service bc
-# with unique id in place of civic address
-# zik files are just zip files, so read_csv can handle them directly.
-# currently only service bc locations are needed
-# Langford: locality 909
-# Smithers: locality 227
-# Dawson Creek: locality 213
-# Kamploops: Locality 420
+# define input and output paths.  assume network path is set
 #------------------------------------------------------------------------------
-
-# define input and output paths
+# set_network_path("<UNC>") # Check out why I need to set the path each time.
 data_folder = use_network_path()
 data_path = os.path.join(data_folder, "data", "raw")
-
-out_folder = os.path.join(data_folder, "data", "source", f"locality_{loc}")
+out_folder = os.path.join(data_folder, "data", "source", "python-test")
 
 if not os.path.exists(out_folder):
     os.makedirs(out_folder)
 
-# get the most recently provisiones files
-files = glob.glob(os.path.join(data_path, "**", "*_no_errors.csv"), recursive=True)
-file_info = [(f, os.path.getmtime(f)) for f in files]
+#------------------------------------------------------------------------------
+# get the most recently provisioned files
+#------------------------------------------------------------------------------
 
+files = glob.glob(os.path.join(data_path, "**", "*_no_errors.csv"), recursive=True)
+files = [f for f in files if os.path.basename(f).startswith("locality_")]
+
+# sort by date created
+file_info = [(f, os.path.getmtime(f)) for f in files]
 sorted_files = sorted(file_info, key=lambda x: x[1], reverse=True)
 
-if len(sorted_files) > 1:
-    file_path = sorted_files[1][0]
-else:
+# and keep only the most recent file for each locality
+unique_files = {}
+for f, mtime in sorted_files:
+    match = re.search(r'locality_(\d{3})', os.path.basename(f))
+    if match:
+        loc_code = match.group(1)
+        if loc_code not in unique_files:
+            unique_files[loc_code] = (f,mtime)
+sorted_files = list(unique_files.values())
+
+if len(sorted_files) < 1:
     raise ValueError("Not enough files matching the pattern 'no_errors.csv'")
 
-# get the locality number from the file path 
-# loc_match = re.search(rf"{re.escape(data_path)}(.*)/locality_([0-9][0-9][0-9])(.*)", file_path)
-# hard code for now as the above line isn't working
-loc=227
+#------------------------------------------------------------------------------
+# Pre-process drive time files for each locality and merge as a single file
+#------------------------------------------------------------------------------
 
-if loc_match:
-    loc = loc_match.group(2)
-else:
-    raise ValueError(f"Could not extract locality from file path: {file_path}")
-
-# read data and do some basic cleaning - we want only the servicebc data
-# explicitly convert data types and rename some columns
+file_path = sorted_files[0][0]
 new_da_servicebc_df = pd.read_csv(file_path, dtype=str)
 
+for i in range(1, len(sorted_files)):
+    file_path = sorted_files[i][0]
+    df = pd.read_csv(file_path, dtype=str)
+    new_da_servicebc_df = pd.concat([new_da_servicebc_df, df], ignore_index=True)
+
 address_sf_with_da = (
-    new_da_servicebc_df
+        new_da_servicebc_df
     .rename(columns=lambda x: re.sub(r'[^A-Za-z0-9_]+', '', x).lower())
     .query("tag == 'servicebc'")
     .rename(columns={"site_albers_x": "address_albers_x", "site_albers_y": "address_albers_y"})
     .assign(
-        daid=lambda df: df["dissemination_block_id"].str[:8],
+            daid=lambda df: df["dissemination_block_id"].str[:8],
         drv_time_sec=lambda df: pd.to_numeric(df["drv_time_sec"]),
         drv_dist=lambda df: pd.to_numeric(df["drv_dist"]),
         address_albers_x=lambda df: pd.to_numeric(df["address_albers_x"]),
@@ -81,4 +89,4 @@ address_sf_with_da = (
 )
 
 # write output to csv
-address_sf_with_da.to_csv(os.path.join(out_folder, f"python_test_address_with_da_loc_{loc}.csv"), index=False)
+address_sf_with_da.to_csv(os.path.join(out_folder, f"drivetime-dist-with_da.csv"), index=False)
