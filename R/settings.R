38d555d3f7ba890b6e7e268a6b45ef1b7ec534e4
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2025-07-10 20:14:12 -0700

    unused constants - temporarily comment out

--- R/settings.R
+++ R/settings.R
@@ -78,19 +78,19 @@ CURRENT_YEAR <- 2025
 LAN_FOLDER <- use_network_path()
 SRC_DATA_FOLDER <- glue("{LAN_FOLDER}/2025 Government Service Locations/data/source/")
 RAW_DATA_FOLDER <- glue("{LAN_FOLDER}/2025 Government Service Locations/data/raw/")
-RAW_POP_FILEPATH <- glue("{RAW_DATA_FOLDER}/statscan/98100015-eng/98100015.csv")
+#RAW_POP_FILEPATH <- glue("{RAW_DATA_FOLDER}/statscan/98100015-eng/98100015.csv")
 DT_DATA_FOLDER <- glue("{LAN_FOLDER}/2025 Government Service Locations/data/raw/nearest_facility_BC")
 
-CROSSWALK_FILEPATH <- glue("{SRC_DATA_FOLDER}/csd-da-db-loc-crosswalk.csv")
-CORRESP_FILEPATH <- glue("{SRC_DATA_FOLDER}/csd-da-db-loc-correspondance.csv")
-DA_SHAPE_FILEPATH <-  glue("{RAW_DATA_FOLDER}/statscan/lda_000b21a_e/lda_000b21a_e.shp")
-DB_SHAPE_FILEPATH <-  glue("{RAW_DATA_FOLDER}/statscan/ldb_000b21a_e/ldb_000b21a_e.shp")
+#CROSSWALK_FILEPATH <- glue("{SRC_DATA_FOLDER}/csd-da-db-loc-crosswalk.csv")
+#CORRESP_FILEPATH <- glue("{SRC_DATA_FOLDER}/csd-da-db-loc-correspondance.csv")
+#DA_SHAPE_FILEPATH <-  glue("{RAW_DATA_FOLDER}/statscan/lda_000b21a_e/lda_000b21a_e.shp")
+#DB_SHAPE_FILEPATH <-  glue("{RAW_DATA_FOLDER}/statscan/ldb_000b21a_e/ldb_000b21a_e.shp")
 SBCLOC_FILEPATH <- glue("{SRC_DATA_FOLDER}/service_bc_locs.csv")
 
 # Output filenames
-OUTPUT_DB_STATS_FILENAME  <- "db_average_times_dist_all_locs.csv"
-OUTPUT_DA_STATS_FILENAME  <- "da_average_times_dist_all_locs.csv"
-OUTPUT_LOC_STATS_FILENAME <- "loc_average_times_dist_all_locs.csv"
+#OUTPUT_DB_STATS_FILENAME  <- "db_average_times_dist_all_locs.csv"
+#OUTPUT_DA_STATS_FILENAME  <- "da_average_times_dist_all_locs.csv"
+#OUTPUT_LOC_STATS_FILENAME <- "loc_average_times_dist_all_locs.csv"
 
 
 SHAPEFILE_OUT <- glue("{SRC_DATA_FOLDER}/shapefiles/")
@@ -99,12 +99,12 @@ VISUALS_OUT <- glue("{LAN_FOLDER}/2025 Government Service Locations/outputs/visu
 TABLES_OUT <- glue("{LAN_FOLDER}/2025 Government Service Locations/outputs/tables")
 
 # Patterns for cleaning
-POP_GUI_PREFIX_PATTERN <- "^2021S[0-9]{4}"
-POP_GUI_BC_PATTERN <- glue("{POP_GUI_PREFIX_PATTERN}59")
-POP_COL_STRIP_PATTERN1 <- "population_and_dwelling_counts_5_"
-POP_COL_STRIP_PATTERN2 <- "_[0-9]$"
-LOCALITY_REGEX_PATTERN <- "[0-9][0-9][0-9]"
-POP_COL_SELECT_PATTERN <- "symbols"
+#POP_GUI_PREFIX_PATTERN <- "^2021S[0-9]{4}"
+#POP_GUI_BC_PATTERN <- glue("{POP_GUI_PREFIX_PATTERN}59")
+#POP_COL_STRIP_PATTERN1 <- "population_and_dwelling_counts_5_"
+#POP_COL_STRIP_PATTERN2 <- "_[0-9]$"
+#LOCALITY_REGEX_PATTERN <- "[0-9][0-9][0-9]"
+#POP_COL_SELECT_PATTERN <- "symbols"
 
 # File patterns
 INPUT_ADDR_DA_PATTERN <- "address_with_da.*"
