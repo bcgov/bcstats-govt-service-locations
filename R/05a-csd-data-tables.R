ff9579ba0cb501f16c59979785c827d31d2b0197
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2025-07-28 15:17:22 -0700

    save scratch work from mock-up

--- R/05a-csd-data-tables.R
+++ R/05a-csd-data-tables.R
@@ -13,3 +13,22 @@
 # limitations under the License.
 
 
+
+
+
+
+
+db_projections_transformed |> filter(gender == 'T') |>
+group_by(year, region_name) %>%
+  summarise(population = sum(population, na.rm = TRUE)) %>%
+  pivot_wider(names_from = year, values_from = population, values_fill = 0) 
+
+db_projections_transformed |> filter(gender == 'T', year == 2025) |>
+mutate(age_grp = case_when(
+    age < 19 ~ "0-19",
+    age >= 19 & age < 65 ~ "19-64",
+    age >= 65 ~ "65+"
+  )) %>%
+  group_by(region_name, age_grp) %>%
+  summarise(population = sum(population, na.rm = TRUE)) %>%
+  pivot_wider(names_from = age_grp, values_from = population, values_fill = 0)
--- R/05b-sbc-data-tables.R
+++ R/05b-sbc-data-tables.R
@@ -13,3 +13,38 @@
 # limitations under the License.
 
 
+drivetime_data_reduced %>%
+group_by(assigned) %>%
+  summarize(n_under_5 = sum(drv_dist < 5.0, na.rm=TRUE), 
+            n_5_to_20 = sum(drv_dist >= 5.0 & drv_dist < 20.0, na.rm=TRUE),
+            n_20_plus = sum(drv_dist >= 20.0, na.rm=TRUE))
+
+drivetime_data_reduced |>
+  distinct(assigned, assignment_method, csdid, dbid) |>
+  # expand drivetime data to include all years of interest for each row
+    expand_grid(
+        tibble(year = rep(unique(db_projections_transformed$year)))
+    ) |>
+  left_join(
+    db_projections_transformed %>% 
+    filter(gender=='T') %>% 
+    select(dbid, year, age, population, total, area_sq_km), 
+    by = c("dbid", 'year')
+    ) |> 
+  filter(!is.na(csdid)) -> df
+
+  df %>%
+  group_by(assigned, year) %>% 
+  summarize(pop = sum(population, na.rm=TRUE)) %>%
+  pivot_wider(names_from = year, values_from = pop, values_fill = 0) 
+
+  df %>% mutate(age_grp = case_when(
+    age < 19 ~ "0-19",
+    age >= 19 & age < 65 ~ "19-64",
+    age >= 65 ~ "65+"
+  )) %>%
+  filter(year == 2025) %>%
+  group_by(assigned, age_grp) %>% 
+  summarize(pop = sum(population, na.rm=TRUE)) %>%
+  pivot_wider(names_from = age_grp, values_from = pop, values_fill = 0)
+
