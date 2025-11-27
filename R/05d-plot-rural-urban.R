0b867fe14d0c293d651554bf7e099044bdadd5d3
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2025-11-27 08:26:02 -0800

    move plotting function to fxns

--- R/05d-plot-rural-urban.R
+++ R/05d-plot-rural-urban.R
@@ -1,56 +1,5 @@
-# -------------------------- DECLARE FUNCTION ----------------------------
-vis_pc <- function(data) {
-  # this function allows you to visualize an region (i.e. popcenter) and it's intersecting blocks (i.e. DB's)
-
-  cat(paste0("Processing plot ", data$id, ": ", data$pcname), "\n")
-  cat(paste0("\t", "Count of DB's: ", length(unique(data$data$dbid)), "\n"))
-
-  sf_data <- st_as_sf(data$data, crs = 3005)
-  bbox <- st_bbox(sf_data$pc_geom) # use db_geom instead, to zoom out from popcenter
-  title <- paste0(
-    "Population Center: ",
-    paste0(data$pcname, collapse = ", "),
-    "\n",
-    "(N) DBID's: ",
-    paste0(length(unique(data$data$dbid)))
-  )
-
-  p <- ggplot2::ggplot() +
-    geom_sf(
-      data = sf_data |> distinct(), # plot boundary one time only
-      aes(geometry = pc_geom, color = "population center"),
-      fill = NA,
-      linewidth = 0.7
-    ) +
-    geom_sf(
-      data = sf_data,
-      aes(geometry = db_geom, fill = rurality),
-      alpha = 0.2,
-      linewidth = 0.1
-    ) +
-    MAP_THEME +
-    coord_sf(
-      datum = st_crs(3005),
-      xlim = c(bbox$xmin - 1000, bbox$xmax + 1000),
-      ylim = c(bbox$ymin - 1000, bbox$ymax + 1000)
-    ) +
-    labs(
-      y = "",
-      x = "",
-      title = title
-    ) +
-    theme(
-      axis.text = element_text(size = 10),
-      plot.title = element_text(size = 12),
-      legend.title = element_blank()
-    ) +
-    scale_color_manual(
-      values = c("population center" = "black")
-    )
-
-  # return the grob object
-  ggplot2::ggplotGrob(p)
-}
+source("R/settings.R")
+source("R/fxns/rural-fxns.R")
 
 # -------------------------- READ DATA -----------------------------------
 db_shapefiles <- st_read(glue("{SHAPEFILE_OUT}/full-db-with-location.gpkg")) |>
